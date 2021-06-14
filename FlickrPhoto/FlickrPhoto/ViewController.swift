bccecd8a2cc9409b7c30a6d19a78003564c05962
nirmala <nirmala.maurya@capgemini.com>
2021-06-14 14:50:18 +1000

    initial Project setup for iOS App.

---
+++ FlickrPhoto/FlickrPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrPhoto
+//
+//  Created by Nirmala on 14/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
